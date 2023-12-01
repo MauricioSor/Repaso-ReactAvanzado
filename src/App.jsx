0b0005727489986a5926fc02ec3bc044c83ec1bc
Mauricio Soria <mauricioutn2017@gmail.com>
2023-12-01 18:41:08 -0300

    Separacion de componentes de navbar , footer e Information

--- src/App.jsx
+++ src/App.jsx
@@ -2,80 +2,31 @@
 import './styles/index.css';
 import './styles/features.css';
 import hero from '../src/assets/hero-img.png';
-import Navbar from './components/Navbar/Navbar';
-import FeatureSection from './components/Features/FeatureSection';
+import Navbar from './common/Navbar/Navbar';
+import FeatureSection from './components/Information/Features/FeatureSection';
+import CoursesSection from './components/Information/Courses/CoursesSection';
+import InformationSection from './components/Information/InformationSection';
+import Footer from './common/Footer/Footer';
 
 const App = () => {
 
   return (
     <>
-    <Navbar/>
-
+      <Navbar />
       <div className='container p-5'>
-        <div className='row'  style={{ justifyContent: 'center', alignItems: 'center'}}>
-            <div className='col-7'>
-                <div style={{fontSize: '60px', fontWeight: 'bold'}}>RollingCode</div>
-                <div style={{fontSize: '30px'}}>Curso de React Avanzado!</div>
-                <button className='btn btn-primary btn-lg mt-4'>VER MAS</button>
-            </div>
-            <div className='col-5'>
-                <img src={hero} alt="header image" width={500}/>
-            </div>
+        <div className='row' style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <div className='col-7'>
+            <div style={{ fontSize: '60px', fontWeight: 'bold' }}>RollingCode</div>
+            <div style={{ fontSize: '30px' }}>Curso de React Avanzado!</div>
+            <button className='btn btn-primary btn-lg mt-4'>VER MAS</button>
+          </div>
+          <div className='col-5'>
+            <img src={hero} alt="header image" width={500} />
+          </div>
         </div>
       </div>
-  <FeatureSection/>
-
-
-      
-      <footer className="py-5 px-5 bg-white container">
-        <div className="row">
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-            </ul>
-          </div>
-
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-            </ul>
-          </div>
-
-          <div className="col-6 col-md-2 mb-3">
-            <h5>Section</h5>
-            <ul className="nav flex-column">
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-            </ul>
-          </div>
-
-          <div className="col-md-5 offset-md-1 mb-3">
-            <form>
-              <h5>Subscribe to our newsletter</h5>
-              <p>Monthly digest of what's new and exciting from us.</p>
-              <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                <label for="newsletter1" className="visually-hidden">Email address</label>
-                <input id="newsletter1" type="text" className="form-control" placeholder="Email address" />
-                <button className="btn btn-primary" type="button">Subscribe</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </footer>
-      
+      <InformationSection/>
+      <Footer/>
     </>
   );
 }
---
+++ src/components/Information/Features/FeatureSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Features from './Features';
+import { Novedades } from '../../../data/Novedades';
+const FeatureSection = () => {
+    return (
+        <div>
+                    <h2 className="pb-2 border-bottom ">NOVEDADES</h2>
+                    <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                        {
+                            Novedades.map((item, index) => (
+                                <Features key={index} item={item} />
+                            ))
+                        }
+                    </div>
+        </div>
+    );
+};
+
+export default FeatureSection;
