4581c20c8dedf1be358884e700c5b3b3f3ce04ca
Mauricio Soria <mauricioutn2017@gmail.com>
2023-12-01 20:18:56 -0300

    task added

---
+++ src/components/Tasks/Task.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TaskCard from './TaskCard';
+import TaskAdd from './TaskAdd';
+
+const Task = () => {
+    return (
+        <div>
+            <TaskCard/>
+            <TaskAdd/>
+        </div>
+    );
+};
+
+export default Task;
